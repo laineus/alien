c6b9aa396290e436f3a8a5312803b5c1ee36f411
laineus <lainen000@gmail.com>
2018-04-22 18:02:27 +0900

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
   },
   module: {
     loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
     ]
   }
 }
